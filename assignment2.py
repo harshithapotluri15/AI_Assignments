0c2b61473299e45c536b22f6a72444c3fbd6a753
harshithapotluri15 <51758416+harshithapotluri15@users.noreply.github.com>
2019-06-12 22:57:03 +0530

    Create assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,21 @@
+g=int(input("enter a number"))
+if(g==ans):
+    print("ur guess is crct")
+elif(g>ans):
+        print("too high")
+else:
+        print("too low") 
+        
+        
+        for i in range(30):
+    if(i%5==0):
+        print(i)
+        
+        :i=1
+il=int(input("enter int limit"))
+while(i*i<=il):
+    a=i*i
+    i=i+1
+
+ns=a
+print(ns)
